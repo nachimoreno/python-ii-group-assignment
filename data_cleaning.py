3f511de74e33f3b41b61b0cea7ab86a6ff5b738f
nachimoreno <nachi.moreno@gmail.com>
2026-03-06 16:18:20 +0100

    feat: Update `data_cleaning.py`to add column dropping, save enriched share prices to a new parquet file, and refactor directory path definitions.

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,9 +2,8 @@ import tomllib
 from pathlib import Path
 import polars as pl
 
-ETL_DIR = 'ETL'
-RAW_DATA_DIR = os.path.join(ETL_DIR, 'data', 'raw')
-PARQUET_DIR = os.path.join(RAW_DATA_DIR, 'parquet')
+RAW_PARQUET_DIR = os.path.join('ETL', 'data', 'raw', 'parquet')
+ENRICHED_PARQUET_DIR = os.path.join('ETL', 'data', 'enriched', 'parquet')
 
 def main():
     print('\nStarting data cleaning...')
@@ -24,8 +23,11 @@ def main():
 
     print('\nReading parquet files...')
     try:
-        companies = pl.read_parquet('ETL/data/raw/parquet/companies.parquet')
-        share_prices = pl.read_parquet('ETL/data/raw/parquet/share_prices.parquet')
+        companies_file_path = os.path.join(RAW_PARQUET_DIR, 'companies.parquet')
+        companies = pl.read_parquet(companies_file_path)
+
+        share_prices_file_path = os.path.join(RAW_PARQUET_DIR, 'share_prices.parquet')
+        share_prices = pl.read_parquet(share_prices_file_path)
     except Exception as e:
         print('Failed to load parquet files. Raising exception...')
         raise e
@@ -36,11 +38,31 @@ def main():
         print(f'Selected companies: {COMPANIES}')
         selected_companies = companies.filter(pl.col('Company Name').is_in(COMPANIES))
         SIMFIN_IDS = [id for id in selected_companies['SimFinId']]
-        selected = share_prices.filter(pl.col('SimFinId').is_in(SIMFIN_IDS)
+        share_prices = share_prices.filter(pl.col('SimFinId').is_in(SIMFIN_IDS))
         print(f'Fetched {len(selected)} rows for {len(COMPANIES)} companies.')
     except Exception as e:
         print('Failed to filter dataframes. Raising exception...')
         raise e
+    print('Successfully filtered dataframes.')
+
+    print('\nDropping unnecessary columns...')
+    try:
+        share_prices = share_prices.drop(pl.col('Dividend'), pl.col('SimFinId'))
+    except Exception as e:
+        print('Failed to drop columns. Raising exception...')
+        raise e
+    print('Successfully dropped columns.')
+
+    print(f'\nSaving to parquet at {ENRICHED_PARQUET_DIR}...')
+    try:
+        file_path = os.path.join(ENRICHED_PARQUET_DIR, 'share_prices_enriched.parquet')
+        share_prices.write_parquet(file_path)
+    except Exception as e:
+        print('Failed to save to parquet. Raising exception...')
+        raise e
+    print('Successfully saved to parquet.')
+    
+    print('\nData cleaning process finished.')
 
 if __name__ == '__main__':
     main()
