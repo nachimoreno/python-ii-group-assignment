4a5b30de29a9044a29d0d85d2211eecea2d95d8d
nachimoreno <nachi.moreno@gmail.com>
2026-03-18 16:09:43 +0100

    feat: Add a model training stage to the pipeline, including scripts for XGBoost classification and regression.

--- manage.py
+++ manage.py
@@ -2,15 +2,17 @@ import argparse
 import src.data_ingestion as data_ingestion
 import src.data_cleaning as data_cleaning
 import src.feature_engineering as feature_engineering
+import src.model_training as model_training
 
 def main():
     data_ingestion.main()
     data_cleaning.main()
     feature_engineering.main()
+    model_training.main()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='ETL Pipeline')
-    parser.add_argument('--stage', type=str, default='all', choices=['ingestion', 'cleaning', 'feature_engineering', 'all'], help='Stage to run')
+    parser.add_argument('--stage', type=str, default='all', choices=['ingestion', 'cleaning', 'feature_engineering', 'model_training', 'all'], help='Stage to run')
     args = parser.parse_args()
     if args.stage == 'ingestion':
         data_ingestion.main()
@@ -18,7 +20,9 @@ if __name__ == '__main__':
         data_cleaning.main()
     elif args.stage == 'feature_engineering':
         feature_engineering.main()
+    elif args.stage == 'model_training':
+        model_training.main()
     elif args.stage == 'all':
         main()
     else:
-        print('Invalid stage. Please choose from ingestion, cleaning, feature_engineering, or all.')
+        print('Invalid stage. Please choose from ingestion, cleaning, feature_engineering, model_training, or all.')
